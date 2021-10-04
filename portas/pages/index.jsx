24449d7c91f228019884c31994b2a8d0cea6642d
Junior Paixao <juniortidesenvolvimento1@gmail.com>
2021-10-04 01:46:50 -0300

    04-10-2021 01:46

--- portas/pages/index.jsx
+++ portas/pages/index.jsx
@@ -1,10 +1,10 @@
-import Presente from "../components/Presente";
+import Porta from "../components/Porta";
 
 
 export default function Home() {
   return (
     <div>
-      <Presente />
+      <Porta />
     </div>
   )
 }
