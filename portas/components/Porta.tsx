4f53b7da243b8a3f44624b4ea402bff10c01894f
Junior Paixao <juniortidesenvolvimento1@gmail.com>
2021-10-04 19:53:12 -0300

    04-10-2021 19:53

---
+++ portas/components/Porta.tsx
@@ -0,0 +1,23 @@
+import styles from "../styles/Porta.module.css"
+import PortaModel from '../model/porta'
+
+interface PortaProps {
+    porta: PortaModel
+}
+
+export default function Porta(props: PortaProps) {
+    const { porta } = props
+    const selecionada = porta.selecionada ? styles.selecionada : ''
+
+    return (
+        <div className={styles.area}>
+            <div className={`${styles.estrutura} ${selecionada}`}>
+                <div className={styles.porta}>
+                    <div className={styles.numero}>{ porta.numero }</div>
+                    <div className={styles.macaneta}></div>
+                </div>
+            </div>
+            <div className={styles.chao}></div>
+        </div>
+    )
+}
--- portas/pages/index.jsx
+++ portas/pages/index.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react"
 import Porta from "../components/Porta";
+import PortaModel from "../model/porta";
 
 
 export default function Home() {
+
+  const [p1, setP1] = useState( new PortaModel(1))
+
   return (
-    <div>
-      <Porta />
+    <div style={{display: "flex"}}>
+      <Porta porta={p1} />
     </div>
   )
 }
