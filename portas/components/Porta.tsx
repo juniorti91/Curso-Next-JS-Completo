f9e16693c2a22bdd8993a7e62cfe69b558188172
Junior Paixao <juniortidesenvolvimento1@gmail.com>
2021-10-04 22:41:48 -0300

    04-10-2021 22:41

--- portas/components/Porta.tsx
+++ portas/components/Porta.tsx
@@ -2,15 +2,18 @@ import styles from "../styles/Porta.module.css"
 import PortaModel from '../model/porta'
 
 interface PortaProps {
-    porta: PortaModel
+    value: PortaModel
+    onChange: (novaPorta: PortaModel) => void
 }
 
 export default function Porta(props: PortaProps) {
-    const { porta } = props
+    const porta  = props.value
     const selecionada = porta.selecionada ? styles.selecionada : ''
 
+    const alternarSelecao = e => props.onChange(porta.alternarSelecao())
+
     return (
-        <div className={styles.area}>
+        <div className={styles.area} onClick={alternarSelecao}>
             <div className={`${styles.estrutura} ${selecionada}`}>
                 <div className={styles.porta}>
                     <div className={styles.numero}>{ porta.numero }</div>
